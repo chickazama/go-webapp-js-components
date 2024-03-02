94d5581d576baad188b1575a3cddc80a53854d8d
Matthew Hope <chickazama@gmail.com>
2024-03-02 22:00:03 +0000

    Implement test Navbar

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,12 @@
+import Navbar from "./components/Navbar.js";
+
+customElements.define("navbar-component", Navbar);
+
 window.addEventListener("load", () => {
     console.log("Page loaded.");
     const main = document.getElementById("main");
+    const testNav = new Navbar();
+    main.appendChild(testNav);
     const testBtn = document.createElement("button");
     testBtn.innerText = "Click Me!";
     testBtn.type = "button";
