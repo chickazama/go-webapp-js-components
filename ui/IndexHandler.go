91f5b1ff6262d8ecf84f0dae39440acc73a0df68
Matthew Hope <matt@matthewhope.co.uk>
2024-03-02 21:27:38 +0000

    Scaffold ui.IndexHandler

---
+++ ui/IndexHandler.go
@@ -0,0 +1,29 @@
+package ui
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type IndexHandler struct {
+}
+
+func (h *IndexHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.get(w, r)
+	}
+}
+
+func (h *IndexHandler) get(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseGlob("./templates/*.go.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.ExecuteTemplate(w, "Index", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
