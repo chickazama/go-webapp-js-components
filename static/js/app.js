d7d2c3ef91f23c730708b848ca63c8228b241b69
Matthew Hope <matt@matthewhope.co.uk>
2024-03-02 21:46:17 +0000

    Add dummy button to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,11 @@
 window.addEventListener("load", () => {
     console.log("Page loaded.");
+    const main = document.getElementById("main");
+    const testBtn = document.createElement("button");
+    testBtn.innerText = "Click Me!";
+    testBtn.type = "button";
+    testBtn.addEventListener("click", () => {
+        console.log("Button clicked.");
+    })
+    main.appendChild(testBtn);
 })
