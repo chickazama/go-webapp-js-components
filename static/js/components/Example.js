cc1f5e855b12ad9eed0139f33816dc75c50cc057
Matthew Hope <chickazama@gmail.com>
2024-03-03 18:39:54 +0000

    Add slottable Example component

---
+++ static/js/components/Example.js
@@ -0,0 +1,24 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <div>
+        <slot name="example">Example Text</p>
+    </div>
+`;
+
+export default class Example extends HTMLElement {
+    shadowRoot;
+    constructor() {
+        super();
+        this.shadowRoot = this.attachShadow({mode: "open"});
+        const tmpl = template.content.cloneNode(true);
+        this.shadowRoot.appendChild(tmpl);
+    }
+
+    connectedCallback() {
+        console.log("Example component connected to DOM.");
+    }
+
+    disconnectedCallback() {
+        console.log("Example component disconnected from DOM.");
+    }
+}
