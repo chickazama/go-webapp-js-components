e396bce0adfd65e02a4ead956f9c73f61e59c207
Matthew Hope <chickazama@gmail.com>
2024-03-03 18:41:40 +0000

    Test slottable component in application JS

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,24 @@
+import Example from "./components/Example.js";
 import Navbar from "./components/Navbar.js";
 
 customElements.define("navbar-component", Navbar);
-
+customElements.define("example-component", Example);
 window.addEventListener("load", () => {
     console.log("Page loaded.");
     const main = document.getElementById("main");
-    const testNav = new Navbar();
-    main.appendChild(testNav);
-    const testBtn = document.createElement("button");
-    testBtn.innerText = "Click Me!";
-    testBtn.type = "button";
-    testBtn.addEventListener("click", () => {
-        console.log("Button clicked.");
-    })
-    main.appendChild(testBtn);
+    const eg = new Example();
+    const h1 = document.createElement("h1");
+    h1.setAttribute("slot", "example");
+    h1.innerText = "Whaddamegunnado";
+    eg.appendChild(h1);
+    main.appendChild(eg);
+    // const testNav = new Navbar();
+    // main.appendChild(testNav);
+    // const testBtn = document.createElement("button");
+    // testBtn.innerText = "Click Me!";
+    // testBtn.type = "button";
+    // testBtn.addEventListener("click", () => {
+    //     console.log("Button clicked.");
+    // })
+    // main.appendChild(testBtn);
 })
