96baef313ae3401af1f8a5860e03ccd45d36cefc
Matthew Hope <chickazama@gmail.com>
2024-03-05 12:23:46 +0000

    Scaffold application JS to create component

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,10 @@
 import Example, { NewExample } from "./components/Example.js";
 
 customElements.define("example-component", Example);
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
     console.log("Page loaded.");
+    const main = document.getElementById("main");
+    const example = NewExample("Welcome!");
+    main.appendChild(example);
+    
 })
