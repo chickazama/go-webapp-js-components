d6ca13638c707d52feb24ebef2bd73c213f0df66
Matthew Hope <chickazama@gmail.com>
2024-03-05 14:43:56 +0000

    Add NewStories endpoint

--- services/fetch.go
+++ services/fetch.go
@@ -6,8 +6,9 @@ import (
 )
 
 const (
-	baseUrl         = "https://hacker-news.firebaseio.com/v0/"
-	maxItemEndpoint = "maxitem.json"
+	baseUrl            = "https://hacker-news.firebaseio.com/v0/"
+	maxItemEndpoint    = "maxitem.json"
+	newStoriesEndpoint = "newstories.json"
 )
 
 func fetch(method, url string, body io.Reader) (*http.Response, error) {
