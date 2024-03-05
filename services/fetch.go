547354cb0640e69201202abff13e7d9a7143f126
Matthew Hope <chickazama@gmail.com>
2024-03-05 13:37:10 +0000

    Write services fetch wrapper

---
+++ services/fetch.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"io"
+	"net/http"
+)
+
+const (
+	baseUrl         = "https://hacker-news.firebaseio.com/v0/"
+	maxItemEndpoint = "maxitem.json"
+)
+
+func fetch(method, url string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return res, err
+	}
+	return res, nil
+}
