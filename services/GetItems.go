e6ea39277d09b920bd6eb07a22fec54fe33d587b
Matthew Hope <chickazama@gmail.com>
2024-03-05 15:30:17 +0000

    Add Channel as argument to services.GetItems

--- services/GetItems.go
+++ services/GetItems.go
@@ -6,25 +6,46 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 )
 
-func GetItems(maxItemID, old int64) {
-	for i := maxItemID; i > old; i-- {
+var (
+	wg sync.WaitGroup
+)
+
+func GetItems(old, new int64, c chan int64) {
+	fmt.Printf("MAX ID: %d\n", new)
+	for i := old + 1; i <= new; i++ {
 		url := fmt.Sprintf("%s/item/%d.json", baseUrl, i)
-		res, err := fetch(http.MethodGet, url, nil)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		defer res.Body.Close()
-		buf, err := io.ReadAll(res.Body)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		var out Item
-		err = json.Unmarshal(buf, &out)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		fmt.Printf("%+v\n", out)
+		wg.Add(1)
+		go func() {
+			for {
+				res, err := fetch(http.MethodGet, url, nil)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				buf, err := io.ReadAll(res.Body)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				res.Body.Close()
+				var out Item
+				err = json.Unmarshal(buf, &out)
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+				if out.ID != 0 {
+					fmt.Printf("[%d]: %s\n", out.ID, out.Type)
+					if out.Type == "story" {
+						c <- out.ID
+					}
+					break
+				} else {
+					fmt.Println("Miss. Retrying...")
+				}
+			}
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 }
