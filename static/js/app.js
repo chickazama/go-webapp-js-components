93bcbed7e122282e546f1cb3a7436a42b52dd6b9
Matthew Hope <chickazama@gmail.com>
2024-03-05 14:01:27 +0000

    Scaffold WS handling on front-end app.js

--- static/js/app.js
+++ static/js/app.js
@@ -6,5 +6,19 @@ window.addEventListener("load", async () => {
     const main = document.getElementById("main");
     const example = NewExample("Welcome!");
     main.appendChild(example);
-    
+    const socket = new WebSocket("ws://localhost:8192/ws");
+    socket.onopen = () => {
+        console.log("Socket connection opened.");
+    }
+    socket.onclose = () => {
+        console.log("Socket connection closed.");
+    }
+    socket.onerror = (e) => {
+        console.log(e);
+    }
+    socket.onmessage = (e) => {
+        const p = document.createElement("p");
+        p.innerText = e.data;
+        main.prepend(p);
+    }
 })
