42775d6ccc99fd8b688e145ca00affb9824dba60
Matthew Hope <chickazama@gmail.com>
2024-03-08 08:24:24 +0000

    Modify IndexViewModel to contain reference to Items

--- ui/IndexViewModel.go
+++ ui/IndexViewModel.go
@@ -1,11 +1,15 @@
 package ui
 
+import "matthewhope/go-webapp-js-components/services"
+
 type IndexViewModel struct {
 	Title string
+	Items []services.Item
 }
 
 func NewIndexViewModel(title string) *IndexViewModel {
 	ret := new(IndexViewModel)
 	ret.Title = title
+	ret.Items = services.GetTopStories()
 	return ret
 }
