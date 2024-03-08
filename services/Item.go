ecba71abd6fbcd181bf43afc16ef0ee93d74100b
Matthew Hope <chickazama@gmail.com>
2024-03-08 08:27:00 +0000

    Fix typos in services.Item fields

--- services/Item.go
+++ services/Item.go
@@ -4,9 +4,9 @@ type Item struct {
 	ID          int64   `json:"id,omitempty"`
 	Author      string  `json:"by,omitempty"`
 	Dead        bool    `json:"dead,omitempty"`
-	Descendants int64   `json:"descendents,omitempty"`
+	Descendants int64   `json:"descendants,omitempty"`
 	Deleted     bool    `json:"deleted,omitempty"`
-	Kids        []int64 `json:"kids"`
+	Kids        []int64 `json:"kids,omitempty"`
 	Parent      int64   `json:"parent,omitempty"`
 	Parts       []int64 `json:"parts,omitempty"`
 	Poll        int64   `json:"poll,omitempty"`
@@ -15,4 +15,9 @@ type Item struct {
 	Title       string  `json:"title,omitempty"`
 	Time        int64   `json:"time,omitempty"`
 	Type        string  `json:"type,omitempty"`
+	URL         string  `json:"url,omitempty"`
+}
+
+func (i *Item) Comments() int {
+	return len(i.Kids)
 }
